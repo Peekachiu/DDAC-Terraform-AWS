580522c156db93200fafb0936bbbb6d6a46dc5ba
Edwin Neoh <edwinneoh@gmail.com>
2025-10-07 21:06:39 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = var.aws_s3_bucket_name
+    key            = "terraform.tfstate"
+    region         = var.aws_region
+    dynamodb_table = var.aws_dynamodb_table_name
+    encrypt        = true
+  }
+  
   required_version = ">=1.5.0"
   required_providers {
     aws = {
