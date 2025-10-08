8b14d1847af12639dc22d0bd96e2e048a3c381c9
Edwin Neoh <edwinneoh@gmail.com>
2025-10-08 14:46:43 +0800

    Comment out "tf-state"
    To avoid re-creating the S3 bucket and DynamoDB

--- main.tf
+++ main.tf
@@ -20,7 +20,10 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-module "tf-state" {
-  source      = "./modules/tf-state"
-  bucket_name = "ddac-tf-state-backend"
-}
+# The "tf-state" module is commented out to avoid re-creating the S3 bucket and DynamoDB table if they already exist.
+# It only needs to be run once to set up the backend infrastructure.
+
+#module "tf-state" {
+#  source      = "./modules/tf-state"
+#  bucket_name = "ddac-tf-state-backend"
+#}
