e65869967844ea01bca6fbc12a0f9d602ac3115c
Edwin Neoh <edwinneoh@gmail.com>
2025-10-08 14:41:43 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
-  #backend "s3" {
-  #  bucket         = "ddac-tf-state-backend"
-  #  key            = "terraform.tfstate"
-  #  region         = "ap-southeast-1"
-  #  dynamodb_table = "terraform-state-locking"
-  #  encrypt        = true
-  #}
+  backend "s3" {
+    bucket         = "ddac-tf-state-backend"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
 
   required_version = ">=1.5.0"
   required_providers {
