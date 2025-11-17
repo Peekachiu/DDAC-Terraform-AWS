c3897af4d5bda0cdbfe17e4d4d504b5162ed2402
Edwin Neoh <edwinneoh@gmail.com>
2025-11-17 20:08:37 +0800

    Update outputs.tf

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -17,9 +17,9 @@ output "instance_profile_name" {
   value       = var.manage_role ? aws_iam_instance_profile.this[0].name : data.aws_iam_instance_profile.external[0].name
 }
 
-output "autoscaling_slr_service_name" {
-  description = "The service name for the Auto Scaling service-linked role (autoscaling.amazonaws.com)."
-  value       = var.manage_role ? aws_iam_service_linked_role.autoscaling[0].aws_service_name : ""
-  sensitive   = false
-}
+# output "autoscaling_slr_service_name" {
+#   description = "The service name for the Auto Scaling service-linked role (autoscaling.amazonaws.com)."
+#   value       = var.manage_role ? aws_iam_service_linked_role.autoscaling[0].aws_service_name : ""
+#   sensitive   = false
+# }
 
