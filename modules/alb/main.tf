d2b77fcb4916be46514310bf26dcc9608a217bb5
Edwin Neoh <edwinneoh@gmail.com>
2025-11-29 00:03:30 +0800

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -29,7 +29,7 @@ resource "aws_lb_target_group" "tg" {
 
   health_check {
     enabled             = true
-    path                = "/api/Dashboard/resident-stats"
+    path                = "/api/"
     matcher             = "200-399"
     interval            = 30
     healthy_threshold   = 3
